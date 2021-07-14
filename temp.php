b205b832d4d8d46778ed2fbd7c417a187bc22e7b
ldi2676 <jw2676@naver.com>
2021-07-14 18:53:16 +0900

    upload

---
+++ temp.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        
+    </head>
+    <body>
+        <?php 
+            set_time_limit(1); 
+            while()
+            {
+                echo date('Y-m-d H:i:s');
+                sleep(10);
+            }
+            
+        ?>
+    </body>
+    
+</html>
